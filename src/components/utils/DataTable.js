42f1db60a7e2a50f2a31c9af60e3b1f0bf553389
AmmyAstro <amrender@dhwaniastro.com>
2026-07-09 18:05:00 +0530

    sr

--- src/components/utils/DataTable.js
+++ src/components/utils/DataTable.js
@@ -1,19 +1,24 @@
-export default function DataTable({ columns, data }) {
-const updatedColumns = [{ header: "Sr No", accessor: "srNo", width: "80px" }, ...columns];
+export default function DataTable({
+  columns,
+  data,
+  startIndex = 0,
+}) {
+  const updatedColumns = [
+    { header: "Sr No", accessor: "srNo", width: "80px" },
+    ...columns,
+  ];
 
-const gridColumns = updatedColumns
-  .map((col) => col.width || "1fr")
-  .join(" ");
+  const gridColumns = updatedColumns
+    .map((col) => col.width || "1fr")
+    .join(" ");
 
   return (
     <div className="w-full bg-white shadow-md rounded-xl border border-gray-200 overflow-hidden">
-      <table className="w-full border-separate  border-spacing-0">
+      <table className="w-full border-separate border-spacing-0">
         <thead className="bg-purple-400 border-b">
           <tr
-            className="border-b  grid"
-            style={{
-              gridTemplateColumns: gridColumns,
-            }}
+            className="border-b grid"
+            style={{ gridTemplateColumns: gridColumns }}
           >
             {updatedColumns.map((col, i) => (
               <th key={i} className="px-2 py-2 text-xs font-semibold">
@@ -27,18 +32,16 @@ const gridColumns = updatedColumns
           {data.map((row, rowIndex) => (
             <tr
               key={row.id || rowIndex}
-              className={`hover:bg-gray-50 border-b border-gray-200 grid `}   style={{
-              gridTemplateColumns: gridColumns,
-            }}
+              className="hover:bg-gray-50 border-b border-gray-200 grid"
+              style={{ gridTemplateColumns: gridColumns }}
             >
               {updatedColumns.map((col, i) => (
                 <td key={i} className="px-1 py-2 text-xs text-center">
-                  {/* 🔥 SR NO logic */}
                   {col.accessor === "srNo"
-                    ? rowIndex + 1
+                    ? startIndex + rowIndex + 1
                     : col.render
-                      ? col.render(row)
-                     : row[col.accessor] ?? "-"}
+                    ? col.render(row)
+                    : row[col.accessor] ?? "-"}
                 </td>
               ))}
             </tr>
